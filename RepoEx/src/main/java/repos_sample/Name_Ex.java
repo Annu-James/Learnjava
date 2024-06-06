d7e568429e41907f4617cded98dfa185a82a1272
jeffk <jeffk@192.168.1.42>
2024-06-06 09:05:46 +0530

    check

---
+++ RepoEx/src/main/java/repos_sample/Name_Ex.java
@@ -0,0 +1,17 @@
+package repos_sample;
+
+public class Name_Ex {
+	public void name()
+	{
+		String a="Solo";
+		int b=456;
+		System.out.println("Name: "+a+" "+"Mark: "+b);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Name_Ex obj=new Name_Ex();
+obj.name();
+	}
+
+}
